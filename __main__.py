a59c7b8e4c81ad176b1b01a13a16f88621b0c0ac
Anonymous4045 <Unanimous4045@proton.me>
2023-06-24 10:48:58 -0500

    Add bad name handling

--- __main__.py
+++ __main__.py
@@ -12,8 +12,7 @@ import shutil
 
 from google_trans_new import google_translator
 
-WELCOME_MESSAGE = """
-Welcome to the File Renamer!
+WELCOME_MESSAGE = """Welcome to the File Renamer!
 This script renames all files in a directory to a given name and translates it for you.
 """
 
@@ -26,28 +25,57 @@ def rename_files(directory) -> None:
     :return: None
     """
     translator = google_translator()
-    for file in pathlib.Path(directory).iterdir():
+    # Use while loop to allow user to repeat file on bad name
+    directory = pathlib.Path(directory)
+
+    # Get list of files in directory
+    files = [file for file in directory.iterdir()]
+
+    i = 0
+    while i < len(files):
+        file = files[i]
+        rename = True
         if file.is_file():
             filename = file.name
             filename_without_extension = filename.split(".")[0]
             translated_filename = translator.translate(
                 filename_without_extension, lang_tgt="en"
             )
+
             new_filename = input(
-                f"File: \n{filename}\nTranslated File: \n{translated_filename}\nNew File Name: "
+                f"File | Translated file\n{filename_without_extension}\n{translated_filename}\nNew File Name: "
             )
+
             if not new_filename:
                 print("Skipping file...")
+                i += 1
                 continue
-            elif new_filename == "exit":
+            if new_filename == "exit":
                 print("Exiting...")
                 break
-            else:
-                # Rename the file
-                new_filename_with_extension = f"{new_filename}.{filename.split('.')[1]}"
+
+            new_filename_with_extension = f"{new_filename}.{filename.split('.')[1]}"
+
+            # Make sure the new filename is unique
+            if directory.joinpath(new_filename_with_extension).exists():
+                print("File already exists. Please choose a different name.\n")
+                rename = False
+
+            # Check for illegal characters in the filename
+            illegal_characters = ["<", ">", ":", '"', "/", "\\", "|", "?", "*"]
+            for character in illegal_characters:
+                if character in new_filename_with_extension:
+                    print(
+                        "Illegal character in filename. Please choose a different name.\n"
+                    )
+                    rename = False
+
+            if rename:
                 new_file_path = file.parent.joinpath(new_filename_with_extension)
                 shutil.move(file, new_file_path)
-                print(f"Renamed file to {new_filename_with_extension}\n")
+                print(f"Renamed file to \"{new_filename_with_extension}\"\n")
+
+                i += 1
 
 
 if __name__ == "__main__":
